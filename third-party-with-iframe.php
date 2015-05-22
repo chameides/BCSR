1a7d0c2571944416aab227937a89e60564e5cebc
MC (laptop) <mchameides@ITSs-MacBook-Pro-3.local>
2015-05-22 13:55:39 -0400

    3rd party files
    blank template
    iframe template
    logos

---
+++ third-party-with-iframe.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simon's Rock Templates, and Style Guide</title>
+
+<?php include '_inc/head-js-css.php'; ?>
+
+  </head>
+  <body>
+
+  
+<?php include '_inc/nav.php'; ?>
+    
+    
+    <div class="container">
+
+    	<div class="row">
+    		<div class="col-xs-12">
+				<h1>Lorem ipsum dolor</h1>
+
+				<h2>Debitis deleniti neque error</h2>
+
+				<h3>Aliquam cumque nobis officia</h3>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, aliquam quam ducimus unde distinctio dolorum excepturi corporis, tempora neque beatae sequi sunt nulla, debitis a perferendis quisquam voluptatibus sit aut.</p>
+				
+				<p><strong>The below is iframed content, demonstrating an implementation of third-party content on site:</strong></p>
+
+				<iframe src="https://bardsimonsrock.hobsonsradius.com/crm/forms/G0SC27G0kA70x6708Nq" onload="window.parent.parent.scrollTo(0,0)" style="width: 730px; border: 0; height: 1200px; position: relative; top: -15px; left: -10px;"></iframe>
+
+    		</div>
+    	</div>
+
+
+
+
+    </div><!-- /.container -->
+
+<?php include '_inc/footer.php'; ?>
+    
+<?php include '_inc/bottom-js.php' ?>
+
+  </body>
+</html>
---
+++ third-party.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simon's Rock Templates, and Style Guide</title>
+
+<?php include '_inc/head-js-css.php'; ?>
+
+  </head>
+  <body>
+
+  
+<?php include '_inc/nav.php'; ?>
+    
+    
+    <div class="container">
+
+    	<div class="row">
+    		<div class="col-xs-12">
+				<h1>Lorem ipsum dolor</h1>
+
+				<h2>Debitis deleniti neque error</h2>
+
+				<h3>Aliquam cumque nobis officia</h3>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, aliquam quam ducimus unde distinctio dolorum excepturi corporis, tempora neque beatae sequi sunt nulla, debitis a perferendis quisquam voluptatibus sit aut.</p>
+				
+    		</div>
+    	</div>
+
+
+
+
+    </div><!-- /.container -->
+
+<?php include '_inc/footer.php'; ?>
+    
+<?php include '_inc/bottom-js.php' ?>
+
+  </body>
+</html>
