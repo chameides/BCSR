caf084dab507a34a22bbadb9df4053c8ba4b6b0e
MC (laptop) <mchameides@ITSs-MacBook-Pro-3.local>
2015-05-26 11:01:29 -0400

    Ebso form test

---
+++ content-library.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="_js/modernizr.custom.js"></script>
+    <title>Campus Landing</title>
+
+<?php include '_inc/head-js-css.php'; ?>
+
+  </head>
+  <body class="has-hero">
+
+  
+<?php include '_inc/nav.php'; ?>
+    <div class="container-fluid page-container">
+
+      
+      <!-- Page Hero -->
+      <div class="row-full-width">
+
+        <div class="page-hero-container campus-landing-bg hero-with-nav">
+          <div class="page-hero text-center">
+            <h1 class="hero-title">Kilpatrick Athletic Center</h1>
+            <p class="hero-text">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti</p>
+          </div>
+        </div>
+
+        <i id="intro_arrow_down" class="fa fa-angle-down"></i>
+      
+      </div>
+      <!-- /Page Hero -->
+
+    
+      <!-- << Section One >> -->
+      <div class="row-full-width">
+        <!-- LEFT COLUMN -->
+        <div class="column-left side-nav" data-spy="affix" data-offset-top="500">
+          <div class="reveal-side-nav"></div>
+          <!-- Subnav -->
+          <ul class="flat-list list-group">
+            <li><div class="list-group-item active section-header">Campus</div></li>
+            <li><a href="#" class="list-group-item">Program Overview</a></li>
+            <li><a href="#" class="list-group-item">Social and Political Studies</a></li>
+            <li><a href="#" class="list-group-item">Languages and Literature</a></li>
+            <li><a href="#" class="list-group-item">Performing and Visual Arts</a></li>
+            <li><a href="#" class="list-group-item">Science, Mathematics, and Computing</a></li>
+            <li><a href="#" class="list-group-item">Beyond the Curriculum</a></li>
+            <li><a href="#" class="list-group-item">Faculty Bios</a></li>
+            <li><a href="#" class="list-group-item">Academic Advising and Support</a></li>
+            <li><a href="#" class="list-group-item">Career Development</a></li>
+          </ul>
+          <!-- /Subnav -->
+        </div>
+        <!-- /LEFT COLUMN-->
+
+        <!-- CENTER COLUMN -->
+        <div class="column-center">
+
+          <!-- Intro -->
+          <div>
+            <h2 class="text-center">Page Headline H2</h2>
+            <p class="intro-paragraph">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui (this should be marketing based copy).</p>
+          </div>
+          <!-- /Intro -->
+
+          <!-- Info Boxes -->
+          <div class="row info-box-container">
+            <div class="info-box-list clearfix">
+              <span class="info-box-item hours">
+                <div class="info-box-text">
+                  <h4>Hours H4</h4>
+                  <p>Monday–Friday 6 a.m.–9 p.m.</p>
+                  <p>Weekends 8 a.m.–6 p.m.</p>
+                  <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum</p>
+                </div>
+              </span>
+              <span class="info-box-item contact">
+                <div class="info-box-text">
+                  <h4>Contact Us H4</h4>
+                  <p>XXX-XXX-XXXX</p>
+                  <p><a href="mailto:lorumipsum@simons-rock.edu">lorumipsum@simons-rock.edu</a></p>
+                </div>
+              </span>
+              <span class="info-box-item rental">
+                <div class="info-box-text">
+                  <h4>Rental Inquiries</h4>
+                  <p><a href="mailto:lorumipsum@simons-rock.edu">lorumipsum@simons-rock.edu</a></p>
+                </div>
+              </span>
+            </div>
+          </div>
+          <!-- /Info Boxes -->
+
+          <!-- Tabbed Image section -->
+          <div class="row">
+
+            <h2 class="text-center">Explore the Kilpatrick H2</h2>
+
+            <div class="accordion-container">
+
+              <div class="panel-group" id="accordion">
+                
+                <div class="panel panel-default">
+                  <div class="panel-heading active">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                      Tuition and Financial Aid
+                    </a>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in">
+                    <div class="accordion-overlay-container">
+                      <img class="accordion-image" src="../_images/pool.jpg" alt="pool">
+                        
+                      <div class="accordion-overlay">
+                        Search for books, articles, and media across nearly all the library’s resources, including JSTOR, Project Muse, EBSCO, Gale, the library catalog, and more.
+Questions? <a href="link">Ask us!</a>
+<!-- EBSCOhost Custom Search Box Begins -->
+<script src="http://support.ebscohost.com/eit/scripts/ebscohostsearch.js" type="text/javascript"></script>
+<form id="ebscohostCustomSearchBox" action="" onsubmit="return ebscoHostSearchGo(this);" method="post" style="width:px; overflow:auto;">
+<input id="ebscohostwindow" name="ebscohostwindow" type="hidden" value="1" />
+<input id="ebscohosturl" name="ebscohosturl" type="hidden" value="http://search.ebscohost.com/login.aspx?direct=true&site=eds- live&scope=site&type=0&custid=s8994393&groupid=main&profid=eds&mode=and&lang=en&authtype=ip,guest,uid" />
+<input id="ebscohostsearchsrc" name="ebscohostsearchsrc" type="hidden" value="db" />
+<input id="ebscohostsearchmode" name="ebscohostsearchmode" type="hidden" value="+" />
+<input id="ebscohostkeywords" name="ebscohostkeywords" type="hidden" value="" />
+<div>
+<div class="col-md-6">
+<input id="ebscohostsearchtext" name="ebscohostsearchtext" type="text" size="23" style="font-size:9pt;padding-left:5px;margin-left:0px;" class="form-control"/>
+</div>
+<div class="col-md-6">
+<input type="submit" value="Find it!" style="font-size:9pt;padding-left:5px;" />
+</div>
+<div id="guidedFieldSelectors">
+<input class="radio" type="radio" name="searchFieldSelector" id="guidedField_0" value="" checked="checked" />
+<label class="label" for="guidedField_0"> Keyword</label>
+<input class="radio" type="radio" name="searchFieldSelector" id="guidedField_1" value="TI" />
+<label class="label" for="guidedField_1"> Title</label>
+<input class="radio" type="radio" name="searchFieldSelector" id="guidedField_2" value="AU" />
+<label class="label" for="guidedField_2"> Author</label>
+</div>
+</div>
+</form>
+<!-- EBSCOhost Custom Search Box Ends -->
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                      Tuition and Financial Aid
+                    </a>
+                  </div>
+                  <div id="collapseTwo" class="panel-collapse collapse">
+                    <div class="accordion-overlay-container">
+                      <img class="accordion-image" src="../_images/lady.jpg" alt="teacher">
+                      <div class="accordion-overlay">
+                        <p class="accordion-text">Second frame caption Copy unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                      Tuition and Financial Aid
+                    </a>
+                  </div>
+                  <div id="collapseThree" class="panel-collapse collapse">
+                    <div class="accordion-overlay-container">
+                      <img class="accordion-image" src="../_images/pool2.jpg" alt="pool">
+                      <div class="accordion-overlay">
+                        <p class="accordion-text">Third frame caption Copy unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapseFour">
+                      Tuition and Financial Aid
+                    </a>
+                  </div>
+                  <div id="collapseFour" class="panel-collapse collapse">
+                    <div class="accordion-overlay-container">
+                      <img class="accordion-image" src="../_images/pool3.jpg" alt="pool">
+                      <div class="accordion-overlay">
+                        <p class="accordion-text">Fourth frame caption Copy unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+          <!-- /Tabbed Image section -->
+        
+        
+			<div class="row">
+				<div class="col-md-6">
+					<h2>If two columns</h2>
+					<p>If two columns of content were to be placed below the tab/accordion control,
+					this is how and where they should be placed</p>
+				</div>        
+				<div class="col-md-6">
+					<img src="//placehold.it/700x500" class="img-responsive">
+				</div>        
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<h2>If one column</h2>
+					<p><img src="//placehold.it/125x125" class="pull-left">
+					<strong>Demonstrating a page-bottom single-column option.</strong> 
+					Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. 
+					Pellentesque ornare sem lacinia quam venenatis vestibulum. 
+					Sed posuere consectetur est at lobortis. Integer posuere erat a ante 
+					venenatis dapibus posuere velit aliquet.
+					Cras justo odio, dapibus ac facilisis in, egestas eget quam. Aenean eu leo quam. 
+					Pellentesque ornare sem lacinia quam venenatis vestibulum. 
+					Sed posuere consectetur est at lobortis. Integer posuere erat a ante 
+					venenatis dapibus posuere velit aliquet.
+					</p>
+				</div>
+			</div>        
+        
+        </div>
+        <!-- /CENTER COLUMN -->
+
+        <!-- RIGHT COLUMN-->
+        <div class="column-right">
+        </div>  
+        <!-- /RIGHT COLUMN -->
+      </div>
+      <!-- << /Section One >> -->
+
+    </div><!-- /container-fluid -->
+
+    <!-- << Section Two >> -->
+      <?php include '_inc/events_carousel.php'; ?>
+    <!-- << /Section Two >> -->
+
+    <div class="row-full-width">
+      <!-- Sidbar Mobile Nav -->
+      <div class="side-nav-mobile">
+        <!-- Subnav -->
+        <ul class="flat-list list-group list-group-mobile">
+          <li><div class="list-group-item active section-header">Campus</div></li>
+            <li><a href="#" class="list-group-item">Program Overview</a></li>
+            <li><a href="#" class="list-group-item">Social and Political Studies</a></li>
+            <li><a href="#" class="list-group-item">Languages and Literature</a></li>
+            <li><a href="#" class="list-group-item">Performing and Visual Arts</a></li>
+            <li><a href="#" class="list-group-item">Science, Mathematics, and Computing</a></li>
+            <li><a href="#" class="list-group-item">Beyond the Curriculum</a></li>
+            <li><a href="#" class="list-group-item">Faculty Bios</a></li>
+            <li><a href="#" class="list-group-item">Academic Advising and Support</a></li>
+            <li><a href="#" class="list-group-item">Career Development</a></li>
+        </ul>
+        <!-- /Subnav -->
+      </div>
+      <!-- /Sidbar Mobile Nav-->
+    </div>
+
+<?php include '_inc/footer.php'; ?>
+    
+<?php include '_inc/bottom-js.php' ?>
+
+  <script type="text/javascript" src="/_js/greensock/TweenMax.min.js"></script>
+  <script type="text/javascript" src="/_js/scrollmagic/jquery.scrollmagic.min.js"></script>
+  <script type="text/javascript" src="/_js/scrollmagic/jquery.scrollmagic.debug.js"></script>
+
+  <script>
+    jQuery('.panel-heading a').click(function() {
+        $('.panel-heading').removeClass('active');
+        $(this).parents('.panel-heading').addClass('active');
+    });
+
+      var controller;
+      $(document).ready(function($) {
+      
+      // init controller
+      controller = new ScrollMagic();
+
+      // Page hero parallax
+      var tween_page_hero = TweenMax.fromTo('.page-hero', 1.5, 
+          { 'opacity' : 1, 'padding-top' : '180px'},
+          { 'opacity' : 0.5, 'padding-top' : '250px'}
+        );
+      // build scene
+      var scene_page_hero = new ScrollScene({duration: 300}).setTween(tween_page_hero).addTo(controller);
+
+      });
+  </script>
+
+  </body>
+</html>
