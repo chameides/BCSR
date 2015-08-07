25148afa0d3bdde936ec5428cfa7d218ccdbc998
MC (laptop) <mchameides@ITSs-MacBook-Pro-2.local>
2015-08-07 11:30:17 -0400

    form: select grade helper text based on month

---
+++ _resources/php/form-grade-date.php
@@ -0,0 +1,11 @@
+<?php 
+  //Helper text based on current month. During the summer, are we asking prospectives to use previous grade or next grade? If change the month, make sure to adjust the math in the form-sumbit script
+  if (date("m")>7) {
+      echo "Select your Current Grade Level";
+  }
+  else {
+      $year = date('Y');
+      echo "Select Grade Completing in $year";
+  }                        
+?>
+
