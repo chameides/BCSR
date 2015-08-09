ea9e9c88bc8b3b7e5b43ae7a917f7cfc9867b4c2
Paul Straw <paulstraw@paulstraw.com>
2015-08-08 19:00:27 -0700

    Moved `.at-a-glance` inside `.container-fluid`

--- simons-rock-at-a-glance.php
+++ simons-rock-at-a-glance.php
@@ -13,202 +13,204 @@
   <body class="has-hero">
     <?php include '_inc/nav.php'; ?>
 
-    <div class="container-fluid page-container at-a-glance">
-      <div class="row-full-width">
-        <div class="page-hero-container at-a-glance-bg">
-          <div class="page-hero page-hero-gateway-alt text-center">
-            <h1 class="hero-title">Simon's Rock at a Glance</h1>
-            <p class="hero-text">A collection of pithy, impressive, and occasionally idiosyncratic stats that begins to capture everything wonderful and life-changing about Simon’s Rock.</p>
+    <div class="container-fluid page-container">
+      <div class="at-a-glance">
+        <div class="row-full-width">
+          <div class="page-hero-container at-a-glance-bg">
+            <div class="page-hero page-hero-gateway-alt text-center">
+              <h1 class="hero-title">Simon's Rock at a Glance</h1>
+              <p class="hero-text">A collection of pithy, impressive, and occasionally idiosyncratic stats that begins to capture everything wonderful and life-changing about Simon’s Rock.</p>
 
-            <i id="intro_arrow_down" class="fa fa-angle-down"></i>
-          </div>
-        </div>
-      </div>
-
-      <div id="location">
-        <div class="container">
-          <div class="row">
-            <h2 class="text-center col-sm-10 col-sm-push-1">“No. 1 Small Town in America”</h2>
-            <p class="col-xs-12 col-md-8 col-md-push-2 text-center">Our home, Great Barrington, Massachusetts, as ranked by Smithsonian Magazine. It’s an easy walk or bike ride from campus.</p>
-          </div>
-
-          <div class="row">
-            <div class="col-xs-6 col-sm-5 col-md-push-1">
-              <h2>275 Acres</h2>
-              <p>Our wooded campus is in the heart of the Berkshires, an area known for its natural beauty and rich cultural life.</p>
+              <i id="intro_arrow_down" class="fa fa-angle-down"></i>
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="container" id="concentrations">
-        <div class="no-margin row text-center">
-          <div class="ribbon"><span>40+ Concentrations</span></div>
+        <div id="location">
+          <div class="container">
+            <div class="row">
+              <h2 class="text-center col-sm-10 col-sm-push-1">“No. 1 Small Town in America”</h2>
+              <p class="col-xs-12 col-md-8 col-md-push-2 text-center">Our home, Great Barrington, Massachusetts, as ranked by Smithsonian Magazine. It’s an easy walk or bike ride from campus.</p>
+            </div>
 
-          <h4>Our Top Majors Include:</h4>
-
-          <div class="major col-xs-6 col-sm-3 icon-major-biology">
-            <h4>Biology</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-photography">
-            <h4>Photography</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-politics">
-            <h4>Politics</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-music">
-            <h4>Music</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-theater">
-            <h4>Theater</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-cross-cultural-relations">
-            <h4>Cross Cultural Relations</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-pre-engineering">
-            <h4>Pre-Engineering</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 icon-major-literary-studies">
-            <h4>Literary Studies</h4>
+            <div class="row">
+              <div class="col-xs-6 col-sm-5 col-md-push-1">
+                <h2>275 Acres</h2>
+                <p>Our wooded campus is in the heart of the Berkshires, an area known for its natural beauty and rich cultural life.</p>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div class="row text-center">
-          <div class="major col-xs-6 col-sm-3 col-sm-push-3 icon-major-psychology">
-            <h4>Psychology</h4>
-          </div>
-          <div class="major col-xs-6 col-sm-3 col-sm-push-3 icon-major-creative-writing">
-            <h4>Creative Writing</h4>
-          </div>
-        </div>
-      </div>
+        <div class="container" id="concentrations">
+          <div class="no-margin row text-center">
+            <div class="ribbon"><span>40+ Concentrations</span></div>
 
-      <div id="our-students">
-        <div class="container">
+            <h4>Our Top Majors Include:</h4>
+
+            <div class="major col-xs-6 col-sm-3 icon-major-biology">
+              <h4>Biology</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-photography">
+              <h4>Photography</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-politics">
+              <h4>Politics</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-music">
+              <h4>Music</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-theater">
+              <h4>Theater</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-cross-cultural-relations">
+              <h4>Cross Cultural Relations</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-pre-engineering">
+              <h4>Pre-Engineering</h4>
+            </div>
+            <div class="major col-xs-6 col-sm-3 icon-major-literary-studies">
+              <h4>Literary Studies</h4>
+            </div>
+          </div>
+
           <div class="row text-center">
-            <h2>Our Students</h2>
-
-            <div class="col-xs-6 col-sm-4">
-              <div class="no-margin row">
-                <h2 class="stat blue col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>400</span></h2>
-                <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Full-Time Students</h5>
-              </div>
+            <div class="major col-xs-6 col-sm-3 col-sm-push-3 icon-major-psychology">
+              <h4>Psychology</h4>
             </div>
-            <div class="col-xs-6 col-sm-4">
-              <div class="no-margin row">
-                <h2 class="stat red col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>16.5</span></h2>
-                <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Average Age of Entering Students</h5>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-4">
-              <div class="no-margin row">
-                <h2 class="stat orange col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>11</span></h2>
-                <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Average Class Size</h5>
-              </div>
-            </div>
-            <div class="col-xs-6 col-sm-4 col-sm-push-2">
-              <div class="no-margin row">
-                <h2 class="stat yellow col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>45</span></h2>
-                <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Number of States our Students Come From</h5>
-              </div>
-            </div>
-            <div class="col-xs-6 col-xs-push-3 col-sm-4 col-sm-push-2">
-              <div class="no-margin row">
-                <h2 class="stat yellow col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>27</span></h2>
-                <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Number of Countries our Students Come From</h5>
-              </div>
-            </div>
-          </div>
-
-          <div class="no-margin row">
-            <div class="col-sm-4 col-sm-push-2">
-              <h4>Students Who Live On Campus</h4>
-            </div>
-            <div class="col-sm-4 col-sm-push-2">
-              <h4>Students Of Color</h4>
-            </div>
-          </div>
-
-          <div class="no-margin row">
-            <div class="col-sm-4 col-sm-push-2">
-              <h4>Male Students</h4>
-            </div>
-            <div class="col-sm-4 col-sm-push-2">
-              <h4>Female Students</h4>
-            </div>
-          </div>
-
-          <div class="no-margin row">
-            <div class="col-sm-4 col-sm-push-2">
-              <h4>Students Receiving Financial Aid</h4>
-            </div>
-            <div class="col-sm-4 col-sm-push-2">
-              <h4>International Students</h4>
+            <div class="major col-xs-6 col-sm-3 col-sm-push-3 icon-major-creative-writing">
+              <h4>Creative Writing</h4>
             </div>
           </div>
         </div>
-      </div>
 
-      <div id="our-faculty">
-        <div class="container">
-          <div class="row">
-            <h2 class="text-center">Our Faculty</h2>
+        <div id="our-students">
+          <div class="container">
+            <div class="row text-center">
+              <h2>Our Students</h2>
 
-            <div>
-              <div class="stat blue col-xs-10 col-md-7 col-md-push-2">
-                <h2>8:1</h2>
-                <h4>Student to faculty ratio</h4>
+              <div class="col-xs-6 col-sm-4">
+                <div class="no-margin row">
+                  <h2 class="stat blue col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>400</span></h2>
+                  <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Full-Time Students</h5>
+                </div>
               </div>
-              <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-ratio"></div>
-
-              <div class="stat orange col-xs-10 col-md-7 col-md-push-2">
-                <h2>95%</h2>
-                <h4>Full-time faculty holding highest degree in their field</h4>
+              <div class="col-xs-6 col-sm-4">
+                <div class="no-margin row">
+                  <h2 class="stat red col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>16.5</span></h2>
+                  <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Average Age of Entering Students</h5>
+                </div>
               </div>
-              <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-cap"></div>
-
-              <div class="stat yellow col-xs-10 col-md-7 col-md-push-2">
-                <h2>24/7</h2>
-                <h4>Availability of our professional residence directors—who live in each residence hall</h4>
+              <div class="col-xs-6 col-sm-4">
+                <div class="no-margin row">
+                  <h2 class="stat orange col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>11</span></h2>
+                  <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Average Class Size</h5>
+                </div>
               </div>
-              <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-lamp"></div>
-
-              <div class="stat red col-xs-10 col-md-7 col-md-push-2">
-                <h2>Once a week</h2>
-                <h4>Number of times you’ll meet with your academic advisor (That’s a minimum. He/she will always be happy to talk to you more)</h4>
+              <div class="col-xs-6 col-sm-4 col-sm-push-2">
+                <div class="no-margin row">
+                  <h2 class="stat yellow col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>45</span></h2>
+                  <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Number of States our Students Come From</h5>
+                </div>
               </div>
-              <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-coffee"></div>
-
-              <div class="stat blue col-xs-10 col-md-7 col-md-push-2">
-                <h2>0</h2>
-                <h4>Number of schools that do what we do</h4>
+              <div class="col-xs-6 col-xs-push-3 col-sm-4 col-sm-push-2">
+                <div class="no-margin row">
+                  <h2 class="stat yellow col-xs-10 col-xs-push-1 col-sm-6 col-sm-push-3"><span>27</span></h2>
+                  <h5 class="stat-description col-xs-12 col-lg-8 col-lg-push-2">Number of Countries our Students Come From</h5>
+                </div>
               </div>
-              <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-logo"></div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div id="smartest-colleges">
-        <div class="container">
-          <div class="row text-center">
-            <h3 class="col-sm-8 col-sm-push-2">“One of America’s smartest colleges.”</h3>
-            <p class="col-sm-10 col-sm-push-1">Business Insider picked Simon’s Rock as a top choice “if you want to be among the most intelligent students in America”</p>
+            <div class="no-margin row">
+              <div class="col-sm-4 col-sm-push-2">
+                <h4>Students Who Live On Campus</h4>
+              </div>
+              <div class="col-sm-4 col-sm-push-2">
+                <h4>Students Of Color</h4>
+              </div>
+            </div>
 
-            <div class="col-xs-12">
-              <div class="page--link">
-                <a class="right-arrow-primary" href="#"><i class="fa fa-angle-right bold"></i> Learn about our sterling reputation and enviable outcomes</a>
+            <div class="no-margin row">
+              <div class="col-sm-4 col-sm-push-2">
+                <h4>Male Students</h4>
+              </div>
+              <div class="col-sm-4 col-sm-push-2">
+                <h4>Female Students</h4>
+              </div>
+            </div>
+
+            <div class="no-margin row">
+              <div class="col-sm-4 col-sm-push-2">
+                <h4>Students Receiving Financial Aid</h4>
+              </div>
+              <div class="col-sm-4 col-sm-push-2">
+                <h4>International Students</h4>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="row-full-width">
-        <div class="solid-bar">
-          <h3 class="text-center">Questions? We would love to talk to you!</h3>
-          <div class="text-center">
-            <a class="btn btn-primary" href="#">Contact Admission</a>
+        <div id="our-faculty">
+          <div class="container">
+            <div class="row">
+              <h2 class="text-center">Our Faculty</h2>
+
+              <div>
+                <div class="stat blue col-xs-10 col-md-7 col-md-push-2">
+                  <h2>8:1</h2>
+                  <h4>Student to faculty ratio</h4>
+                </div>
+                <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-ratio"></div>
+
+                <div class="stat orange col-xs-10 col-md-7 col-md-push-2">
+                  <h2>95%</h2>
+                  <h4>Full-time faculty holding highest degree in their field</h4>
+                </div>
+                <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-cap"></div>
+
+                <div class="stat yellow col-xs-10 col-md-7 col-md-push-2">
+                  <h2>24/7</h2>
+                  <h4>Availability of our professional residence directors—who live in each residence hall</h4>
+                </div>
+                <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-lamp"></div>
+
+                <div class="stat red col-xs-10 col-md-7 col-md-push-2">
+                  <h2>Once a week</h2>
+                  <h4>Number of times you’ll meet with your academic advisor (That’s a minimum. He/she will always be happy to talk to you more)</h4>
+                </div>
+                <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-coffee"></div>
+
+                <div class="stat blue col-xs-10 col-md-7 col-md-push-2">
+                  <h2>0</h2>
+                  <h4>Number of schools that do what we do</h4>
+                </div>
+                <div class="icon col-xs-2 col-md-1 col-md-push-2 icon-faculty-logo"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="smartest-colleges">
+          <div class="container">
+            <div class="row text-center">
+              <h3 class="col-sm-8 col-sm-push-2">“One of America’s smartest colleges.”</h3>
+              <p class="col-sm-10 col-sm-push-1">Business Insider picked Simon’s Rock as a top choice “if you want to be among the most intelligent students in America”</p>
+
+              <div class="col-xs-12">
+                <div class="page--link">
+                  <a class="right-arrow-primary" href="#"><i class="fa fa-angle-right bold"></i> Learn about our sterling reputation and enviable outcomes</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row-full-width">
+          <div class="solid-bar">
+            <h3 class="text-center">Questions? We would love to talk to you!</h3>
+            <div class="text-center">
+              <a class="btn btn-primary" href="#">Contact Admission</a>
+            </div>
           </div>
         </div>
       </div>
