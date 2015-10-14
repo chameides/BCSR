baaf17ecd4912a139cfa8fb6d7c09f35b9d68bde
MC (laptop) <mchameides@ITSs-MacBook-Pro-4.local>
2015-10-14 13:57:58 -0400

    Directory iFrame

---
+++ directory-iframe.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <title>Directory | Bard College at Simon's Rock</title>
+      <meta charset="utf-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+      <meta name="google-site-verification" content="bWTP3Dh4CP53ONDLO3NaQHnRoXkWU8d-KeFWlislfiQ" />
+      <meta name="google-site-verification" content="URa0nbYkyf6MKbE6FeYFywbz0bIoceyAfhIYpYIGHFM" />
+
+      <!-- Bootstrap -->
+      <link href="_css/iframe-compiled.css" rel="stylesheet"/>
+      <link href="http://simons-rock.edu/_resources/css/app.css" rel="stylesheet"/>
+      <link href="http://simons-rock.edu/_resources/css/app.css" rel="stylesheet"/>
+   </head>
+  <body class="iframe">
+		<form>
+      <div class="form-group col-md-12 input-and-button-wrapper">
+        <div class="input-and-button-container">
+          <div class="input-container">
+            <input class="form-control" placeholder="Name or part of name" type="text"/>
+          </div><!--close input-container -->
+          <input type="submit" value="Search" class="btn btn-primary" style="display: inline-block; float: right;"/>
+        </div><!--close input-and-button container -->
+      </div><!--close wrapper -->
+    </form>
+  </body>
+</html>
