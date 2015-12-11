491e24453337986d7aecc5942fcb6cb2350c036a
MC (laptop) <mchameides@ITSs-MacBook-Pro-4.local>
2015-12-11 14:10:05 -0500

    gatekeeper draft

---
+++ gatekeeper.php
@@ -0,0 +1,245 @@
+<!--change log
+1. Start with Gateway
+2. Remove php include '_inc/nav.php'; 
+3. Remove Secondary Nav (left)
+4. Remove Calendar Header and Carousel
+5. Remove Footer
+6. Add navbar-brand-gateway
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="_js/modernizr.custom.js"></script>
+    <title>Gatekeeper</title>
+
+<?php include '_inc/head-js-css.php'; ?>
+
+  </head>
+  <body class="has-hero">
+
+
+
+    <div class="container-fluid page-container">
+
+      <!-- Page Hero -->
+      <div class="row-full-width">
+      <div class="navbar-brand-gateway">
+        <a href="/" title="Bard College at Simon's Rock">
+          <img src="/_images/logos/logo_2012_primary_unstacked_mediumRED.png" alt="Bard College at Simon's Rock, the Early College">
+        </a>
+      </div>
+
+        <div class="page-hero-container gateway-bg">
+          <div class="page-hero page-hero-gateway-alt text-center">
+            <h1 class="hero-title">Headline</h1>
+            <p class="hero-text">Supporting Copy</p>
+          </div>
+        </div>
+
+        <i id="intro_arrow_down" class="fa fa-angle-down"></i>
+      
+      </div>
+      <!-- /Page Hero -->
+
+    
+      <!-- << Section One >> -->
+      <div class="row-full-width">
+
+        <!-- LEFT COLUMN -->
+        
+        <!-- /LEFT COLUMN-->
+
+        <!-- CENTER COLUMN -->
+        <div class="column-center">
+
+          <!-- Class News -->
+          <div class="row image-list-section">
+            <div class="col-xs-3 col-sm-3">
+              <a href="#" class="image-thumb">
+                <img src="../_images/class-news.jpg" alt="class news">
+                <div class="image-text-overlay-container">
+                  <span class="image-text-overlay text-center">Class News!</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-9 col-sm-9">
+              <div class="col-sm-12">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui (this should be marketing based copy).</p>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- /Class News -->
+          <!-- Get Involved -->
+          <div class="row image-list-section">
+            <div class="col-xs-3">
+              <a href="#" class="image-thumb">
+                <img src="../_images/get-involved.jpg" alt="get involved">
+                <div class="image-text-overlay-container">
+                  <span class="image-text-overlay text-center">Get Involved</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-9">
+              <div class="col-sm-12">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui (this should be marketing based copy).</p>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+              <div class="col-md-6">
+              </div>
+            </div>
+          </div>
+          <!-- Get Involved -->
+          <!-- Resources -->
+          <div class="row image-list-section">
+            <div class="col-xs-3">
+              <a href="#" class="image-thumb">
+                <img src="../_images/resources.jpg" alt="recources">
+                <div class="image-text-overlay-container">
+                  <span class="image-text-overlay text-center">Resources for Alumni</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-9">
+              <div class="col-sm-12">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui (this should be marketing based copy).</p>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- /Resources -->
+          <!-- Give Back -->
+          <div class="row image-list-section">
+            <div class="col-xs-3">
+              <a href="#" class="image-thumb">
+                <img src="../_images/give-back.jpg" alt="give back">
+                <div class="image-text-overlay-container">
+                  <span class="image-text-overlay text-center">Give Back</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-9">
+              <div class="col-sm-12">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui (this should be marketing based copy).</p>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6">
+              </div>
+            </div>
+          </div>
+          <!-- /Give Back -->
+          <!-- Connect -->
+          <div class="row image-list-section">
+            <div class="col-xs-3">
+              <a href="#" class="image-thumb">
+                <img src="../_images/connect.jpg" alt="connect">
+                <div class="image-text-overlay-container">
+                  <span class="image-text-overlay text-center">Connect on Social Media</span>
+                </div>
+              </a>
+            </div>
+            <div class="col-xs-9">
+              <div class="col-sm-12">
+                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui (this should be marketing based copy).</p>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-6">
+                <ul class="arrow-list">
+                  <li><a href="#">Natus error sit voluptatem</a></li>
+                  <li><a href="#">Doloremque laudantium</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- /Connect -->
+
+        </div>
+        <!-- /CENTER COLUMN -->
+
+        <!-- RIGHT COLUMN-->
+        <div class="column-right">
+        </div>  
+        <!-- /RIGHT COLUMN -->
+
+      </div>
+      <!-- << /Section One >> -->
+    
+      <footer class="container-fluid" id="footer-gatekeeper" >
+        <div class="container">
+          Accredited by the New England Association of Schools and Colleges (NEASC) | <a href="/student-life/map-and-directions/index.php" id="Footer_AddressMap">84 Alford Rd., Great Barrington, MA 01230</a> | <a href="mailto:info@simons-rock.edu" id="Footer_Connect_InfoEmail">info@simons-rock.edu</a> | <a href="tel:413-644-"4400">413-644-4400</a> | Copyright © 2015 Bard College at Simon's Rock
+        </div>
+      </footer
+
+    </div><!-- /container-fluid -->
+
+    
+
+
+    
+<?php include '_inc/bottom-js.php' ?>
+
+  <script type="text/javascript" src="/_js/greensock/TweenMax.min.js"></script>
+  <script type="text/javascript" src="/_js/scrollmagic/jquery.scrollmagic.js"></script>
+  <script type="text/javascript" src="/_js/scrollmagic/jquery.scrollmagic.debug.js"></script>
+
+  <script>
+
+    var controller;
+    $(document).ready(function($) {
+    
+      // init controller
+      controller = new ScrollMagic();
+
+      // Page hero parallax
+      var tween_page_hero = TweenMax.fromTo('.page-hero', 1.5, 
+          { 'opacity' : 1, 'padding-top' : $('.page-hero').css('padding-top')},
+          { 'opacity' : 0.5, 'padding-top' : (parseInt($('.page-hero').css('padding-top'))+60) +'px'}
+      );
+      // build scene
+      var scene_page_hero = new ScrollScene({duration: 300}).setTween(tween_page_hero).addTo(controller);
+    });
+  </script>
+
+  </body>
+</html>
