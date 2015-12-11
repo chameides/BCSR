ea196708d6595c95c58c61d14d60c653660a5901
MC (laptop) <mchameides@ITSs-MacBook-Pro-4.local>
2015-12-11 11:44:41 -0500

    conform 5 signs form

--- contact-form-fields-5signs.php
+++ contact-form-fields-5signs.php
@@ -9,11 +9,11 @@
 		<div id="parentName">
 			<div class="form-group col-md-6"><label for="parentFirstName">First Name*</label> <input id="parentFirstName" class="form-control" maxlength="40" name="parentFirstName" required="required" type="text" /> <!-- use max-length based on CRM import limits--></div>
 			<div class="form-group col-md-6"><label for="parentLastName">Last Name*</label> <input id="parentLastName" class="form-control" maxlength="80" name="parentLastName" required="required" type="text" /></div>
-			<div class="form-group col-md-12"><label for="txtEmail">Email address*</label> <input id="parentEmailSelf" class="form-control" maxlength="100" name="parentEmailSelf" required="required" type="email" placeholder="Enter email" /></div>
+			<div class="form-group col-md-12"><label for="txtEmail">Email address*</label> <input id="parentEmailSelf" class="form-control" maxlength="100" name="parentEmailSelf" required="required" type="email" /></div>
 		</div>
 		<div class="form-group col-md-6"><label for="txtFirstName"><span id="childFirst">Child&rsquo;s </span>First Name*</label> <input id="txtFirstName" class="form-control" maxlength="40" name="txtFirstName" required="required" type="text" /></div>
 		<div class="form-group col-md-6"><label for="txtLastName"><span id="childLast">Child&rsquo;s </span>Last Name*</label> <input id="txtLastName" class="form-control" maxlength="80" name="txtLastName" required="required" type="text" /></div>
-		<div id="emailAddress" class="form-group col-md-12"><label for="txtEmail">Email address*</label> <input id="txtEmail" class="form-control" maxlength="100" name="txtEmail" required="required" type="email" placeholder="Enter email" /></div>
+		<div id="emailAddress" class="form-group col-md-12"><label for="txtEmail">Email address*</label> <input id="txtEmail" class="form-control" maxlength="100" name="txtEmail" required="required" type="email"  /></div>
 		<div id="DOB">
 			<div class="form-group col-sm-6"><label for="birth_month"><span id="childDOB">Child&rsquo;s </span>Date of Birth* <span class="sr-only">(Month)</span></label><select id="birth_month" class="form-control" name="birth_month" required="required">
 				<option value="">Month</option>
@@ -91,7 +91,7 @@
 				</select>
 			</div>
 		</div>
-		<div class="form-group col-sm-12">
+		<div class="form-group col-sm-12 col-md-12">
 			<label for="gradeLevel">Grade Level*</label>
 			<select class="form-control" id="gradeLevel" name="gradeLevel" required="required">
 				<option value="">
@@ -110,13 +110,13 @@
 				<option value="12">12th Grade</option>
 			</select>
 		</div>
-		<div id="note-container" class="col-sm-12 form-group">
+		<div id="note-container" class="col-sm-12 col-md-12 form-group">
 			<label for="note">Tell us about yourself</label> <textarea id="note" class="form-control" name="note"></textarea>
 		</div>
 		<div id="chxInfoByErr" class="col-sm-12"><label>I'd like to receive information by</label></div>
 		<div class="checkbox col-sm-12"><label> <input id="chxInfoByEmail" class="chxInfoBy" checked="checked" name="chxInfoByEmail" type="checkbox" value="Email" /> Email </label></div>
 		<div class="checkbox col-sm-12"><label> <input id="chxInfoByPhone" class="chxInfoBy" name="chxInfoByPhone" type="checkbox" value="Phone" /> Phone </label></div>
-		<div id="grpPhone" class="form-group checkbox-sub col-sm-6"><label for="txtPhone">Primary Phone Number</label> <input id="txtPhone" class="form-control" name="txtPhone" type="text" /></div>
+		<div id="grpPhone" class="form-group checkbox-sub col-sm-6"><label for="txtPhone">Phone Number</label> <input id="txtPhone" class="form-control" name="txtPhone" type="text" /></div>
 		<div id="grpPhoneType" class="form-group checkbox-sub col-sm-6"><label for="drpPhoneType">Phone Type</label>
 			<select id="drpPhoneType" class="form-control" name="drpPhoneType" required="required">
 				<option value="">- Select Phone Type -</option>
@@ -423,13 +423,13 @@
 		<div id="grpZip" class="form-group checkbox-sub col-sm-6"><label for="txtZipOrPostal"><span id="lblZipCode">Zip Code</span><span id="lblPostalCode">Postal Code:</span></label> <input id="txtZipOrPostal" class="form-control" maxlength="15" name="txtZipOrPostal" type="text" /></div>
 		<div id="contactParent">
 			<div class="checkbox col-sm-12"><label> <input id="contactParentInput" class="chxInfoBy" name="contactParentInput" type="checkbox" value="ContactParent" /> Contact my parent </label></div>
-			<div id="parentEmailContainer" class="form-group checkbox-sub col-sm-12"><label for="parentEmail">Parent email address</label> <input id="parentEmail" class="form-control" maxlength="100" name="parentEmail" type="text" placeholder="Enter email" /></div>
+			<div id="parentEmailContainer" class="form-group checkbox-sub col-sm-12"><label for="parentEmail">Parent email address</label> <input id="parentEmail" class="form-control" maxlength="100" name="parentEmail" type="text" /></div>
 		</div>
 			<?php
 						// replace php with xsl-freindly code in CMS <php>
 							include $_SERVER['DOCUMENT_ROOT']. '/_resources/php/form-url.php';
 						//</php>
 					?>
-		<div class="form-group col-sm-12"><input id="RFI_btn_submit" class="btn btn-primary" type="submit" value="Submit" /></div>
+		<div class="form-group col-sm-12"><input id="RFI_btn_submit" class="btn btn-primary" type="Submit" value="Get the Checklist" /></div>
 	</form>
 </div>
