7040c6277a42e3fff3cf94fdf0b08005e082ca6d
MC (laptop) <mchameides@ITSs-MacBook-Pro-4.local>
2016-05-16 18:53:31 -0400

    add contact form pages

---
+++ contact-form-thanks-again.php
@@ -0,0 +1,163 @@
+<?php
+  session_start();
+  if (isset($_SESSION['addressInternationalExists']) && $_SESSION['addressInternationalExists'] == 'true') {
+  	//redirect to thank you page with address follow form
+  	header('Location: /contact-form-thanks-international.php');
+  }
+  else {
+  	//reset variables
+		session_unset();
+		session_destroy();
+		session_write_close();
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simon's Rock: Contact Form</title>
+
+<?php include '_inc/head-js-css.php'; ?>
+
+<style>
+.checkbox-sub {
+	padding-left:35px;
+}
+.checkbox-sub + .checkbox {
+	margin-top:-5px;
+}
+label.error {
+	color:red;
+	font-size:12px;
+	font-weight: normal;
+	line-height: 130%;
+	margin-top: 5px;
+	display: block;
+}
+</style>
+
+  </head>
+  <body>
+
+  
+<?php include '_inc/nav.php'; ?>
+    
+    
+    <div class="container">
+	<form id="contactForm">
+
+
+<h1 id="form_elements" class="sg-heading">Thank You Again</h1>
+<div class="row">
+
+	<div class="col-md-6">
+		<p>Display Variables</p>
+	    <?php 
+	      echo "<p>formSource: " . $_SESSION['formSource'] . "</p>";
+	      print_r($_SESSION);
+
+	    ?>
+		<p>We have recieved your form submission.
+		You should be on the lookout for something.
+		Also, check out our website.</p>
+	</div>
+
+	<div class="col-md-6">
+	
+
+	</div><!-- col-md-6 -->
+</div><!-- row -->
+
+
+<hr />
+
+</form>
+
+    </div><!-- /.container -->
+<?php include '_inc/footer.php'; ?>
+    
+<?php include '_inc/bottom-js.php' ?>
+
+<script src="_js/validation/jquery.validate.min.js"></script>
+
+<script src="_js/validation/additional-methods.min.js"></script>
+
+<script>
+
+
+
+
+$( document ).ready(function() {
+
+	$.validator.setDefaults({
+		submitHandler: function() {
+			alert("submitted!");
+		}
+	});
+
+	$("#contactForm").validate({
+		rules: {
+			txtFirstName: {
+				required: true
+			},
+			txtLastName: {
+				required: true
+			},
+			txtEmail: {
+				required: true,
+				email: true
+			},
+			txtPhone: {
+				required: '#chxInfoByPhone:checked'
+			},
+			drpCountry: {
+				required: '#chxInfoByMail:checked'
+			},
+			txtAddress1: {
+				required: '#chxInfoByMail:checked'
+			},
+			txtZipOrPostal: {
+				required: '#chxInfoByMail:checked'
+			}
+		}
+	});
+
+	$('.checkbox-sub, #lblPostalCode').hide();
+	
+	$('#chxInfoByPhone').click(function(){
+		if ( $(this).is(':checked') ) {
+			$('#grpPhone').fadeIn();
+		} else {
+			$('#grpPhone').fadeOut();
+		}
+	});
+
+	$('#chxInfoByMail').click(function(){
+		if ( $(this).is(':checked') ) {
+			$('#grpCountry, #grpAddress1, #grpAddress2, #grpZip').fadeIn();
+		} else {
+			$('#grpCountry, #grpAddress1, #grpAddress2, #grpZip').fadeOut();
+		}
+	});
+	
+	$('#drpCountry').change(function(){
+		var countryval = $(this).val();
+		if ( countryval == 'United States' ) {
+			$('#lblZipCode').fadeIn();
+			$('#lblPostalCode').hide();
+		} else {
+			$('#lblZipCode').hide();
+			$('#lblPostalCode').fadeIn();
+		}
+	
+	});
+	
+	
+
+});
+</script>
+
+  </body>
+</html>
