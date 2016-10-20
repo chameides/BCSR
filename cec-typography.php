84366ebb6af00e423f3d6ba2632669041dbf58cf
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2016-10-20 16:26:08 -0400

    mobile typography

--- cec-typography.php
+++ cec-typography.php
@@ -18,9 +18,9 @@
 <div class="container-fluid page-container">
   <div class="column-center"> 
     <h1 class="header-page text-center">Typography</h1>
-    <p>The body copy is set in Helvetica Neue Condensed. Clean, contemporary, and yet neutral. Headers are set in Freight Neo. the Sans Serif complements the contemporary feel of the body copy. Yet, the typeface also adds a serious, yet warm feel. The notches and mixed-width reference humanism and and the renaissance.</p>
+    <p>The body copy is set in Helvetica Neue Condensed - clean, contemporary, and yet neutral. Headers are set in Freight Neo. The Sans Serif complements the contemporary feel of the body copy. The notches and mixed-width reference humanism and the renaissance, creating a serious, yet warm feel.</p>
 
-    <h2 class="text-center">Table of Contents</h2>
+    <h2>Table of Contents</h2>
     	<div class="col-md-4">
     		<ul>
 	    		<li><a href="#headings">Headings</a></li>
