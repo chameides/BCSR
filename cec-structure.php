617327d8f5d1b3619749990bff626bb964e053ca
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2016-11-02 14:39:00 -0400

    notes on cec structure

--- cec-structure.php
+++ cec-structure.php
@@ -44,7 +44,16 @@
           <h2>Structure</h2>
           <p>The top navigation features Bard red, key navigation structure, and text treatment of the organization name. On smaller screen sizes, the menu is revealed with the 3-dash 'hamburger' menu. The bottom navigation features top tasks.</p> 
           <h2>Information Architecture</h2>
-          The navigation of the site is separated into four branches, plus two tasks contact us and search. 
+          <h3>Primary</h3>
+          <p>The top navigation of the site is separated into four branches, plus two tasks contact us and search. The four branches are consistent with the approved strategy. Each branch references a key aspect of CEC we want to provide and they also cover the scope of the information.</p>
+          <h3>Secondary</h3>
+          <p>The secondary navigation provides context for where this page fits within the larger structure. Users can browse up and across the file tree. On smaller screens the navigation drops below the content and above the footer.</p>
+          <h3>Tasks</h3>
+          <p>Footer navigation gives key tasks consistent with the approved strategy. Tasks are organized into categories so as to make tasks browseable as well as to highlight key messages about CEC. The footer also includes contact information.</p>   
+
+
+          top navigation of the site is separated into four branches, plus two tasks contact us and search. The four branches are consistent with the approved strategy. Each branch references a key aspect of CEC we want to provide and they also cover the scope of the information.</p>
+
 
           <h2>Additional Features</h2>
           <p>We can and will build out more parts including:</p> 
@@ -52,10 +61,37 @@
             <li>Actual links for each part</li>
             <li>Refinement of the animation and transitions</li>
             <li>Refinement of spacing</li>
-            <li>Optimize the breakpoint between mobile and desktop menu</li>
             <li>Swap out logo</li>
+            <li>Update contact information</li>
             <li>Hero graphic version</li>
           </ul>
+
+          <h2>Technical Notes</h2>
+          <p>The JavaScript is branched from the Simon's Rock repository. The original script was broken up into parts (modules). Alternate and additional modules were created specific for the needs of this site. Then two versions of script, each compiled in grunt with the appropriate modules.</p>
+
+          <pre>
+concat:   
+  app_bcsr:
+    src: [
+      '_js/_js-not-minified/app/app-part1.js',
+      '_js/_js-not-minified/app/show-search-bcsr.js',
+      '_js/_js-not-minified/app/app-part2.js',
+      ]
+    dest: 
+      '_js/_js-not-minified/app/app-bcsr-combined.js'
+  app_cec:  
+    src: [
+      '_js/_js-not-minified/app/app-part1.js',
+      '_js/_js-not-minified/app/show-search-cec.js',
+      '_js/_js-not-minified/app/app-part2.js',
+      ]
+    dest: 
+      '_js/_js-not-minified/app/app-cec-combined.js' 
+          </pre>
+          
+
+
+
         </div><!-- main-content-->
       </div><!--fluid container-->
       <div class="row-full-width">
