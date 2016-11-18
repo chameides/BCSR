59b83201f636566748b2d888e64dd0c1ae7c9591
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2016-11-18 11:18:56 -0500

    alt on footer
    typography comparison

--- _inc/footer-cec.php
+++ _inc/footer-cec.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row footer-main">
 			<div class="col-xs-12">
-				<h4 class="footer-logo"><a href="#" id="Footer_SimonsRockLogo"><img src="/_images/cec-logo-stacked-white.png"></a></h4>
+				<h4 class="footer-logo"><a href="#" id="Footer_SimonsRockLogo"><img src="/_images/cec-logo-stacked-white.png" alt="Bard Center for Early College"></a></h4>
 				<p><a href="">Contact Us</a> | 
 				<a href="">News</a> | 
 				<a href="">Donate</a></p>
@@ -12,7 +12,7 @@
 			<div class="col-xs-12">
 				<ul class="contact-info-list small">
 					<li>Copyright © 2016 Bard Center for Early College</li>
-					<li class="footer-logo-secondary"><h4><a href="https://bard.edu" id="Footer_SimonsRockLogo"><img src="/_images/cec/icon/logo-bard-gray.png"></a></h4></li>		
+					<li class="footer-logo-secondary"><h4><a href="https://bard.edu" id="Footer_SimonsRockLogo"><img src="/_images/cec/icon/logo-bard-gray.png" alt="Bard College"></a></h4></li>		
 				</ul>	
 			</div>
 		</div>
