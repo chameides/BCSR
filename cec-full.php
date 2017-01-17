743ee834f14cb077211a48ee069f0a99ae3d28b6
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2017-01-17 10:26:48 -0500

    change our network images and url

--- cec-full.php
+++ cec-full.php
@@ -29,7 +29,7 @@
         <section class="slat-network section-full-screen">
           <div class="container abscontainer">
             <div class="intro_sub">
-              <img src="/_images/cec/map-network.png" class="hidden-sm hidden-md hidden-lg">
+              <img src="/_images/cec/map-network/our-network-770.png" class="hidden-sm hidden-md hidden-lg">
               <h1 class="hero-title">Our Network</h1>
               <p class="hero-text">We run a network of early college programs, giving unprecedented access to the liberal arts.<br><br>
               </p>
