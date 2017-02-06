68b2f9876def3a9f8a586ed6f3f9efe087320c5a
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2017-02-06 13:41:04 -0500

    optimize events carousel

--- _resources/php/events_carousel.php
+++ _resources/php/events_carousel.php
@@ -1,13 +1,14 @@
 <?php
-	parse_str($_SERVER['QUERY_STRING']);
-	
-	if(isset($_GET["feed"])): $feed = $_GET["feed"]; elseif(!isset($feed)): $feed = "http://".$_SERVER[HTTP_HOST]."/events/rss/databuilder.php"; endif;
-	if(isset($_GET["limit"])): $limit = intval($_GET["limit"]); elseif(!isset($limit)): $limit = 5; endif;
-	if(isset($_GET["buttonUrl"])): $buttonUrl = $_GET["buttonUrl"]; elseif(!isset($buttonUrl)): $buttonUrl = $_SERVER[HTTP_HOST]."/events/"; endif;
-	if(isset($_GET["buttonText"])): $buttonText = $_GET["buttonText"]; elseif(!isset($buttonText)): $buttonText = "View All"; endif;
-
+    ini_set('error_reporting', E_ALL);
+    parse_str($_SERVER['QUERY_STRING']);
+    
+    if(isset($_GET["feed"])): $feed = $_GET["feed"]; elseif(!isset($feed)): $feed = "http://".$_SERVER[HTTP_HOST]."/events/rss/databuilder.php"; endif;
+    if(isset($_GET["limit"])): $limit = intval($_GET["limit"]); elseif(!isset($limit)): $limit = 5; endif;
+    if(isset($_GET["buttonUrl"])): $buttonUrl = $_GET["buttonUrl"]; elseif(!isset($buttonUrl)): $buttonUrl = $_SERVER[HTTP_HOST]."/events/"; endif;
+    if(isset($_GET["buttonText"])): $buttonText = $_GET["buttonText"]; elseif(!isset($buttonText)): $buttonText = "View All"; endif;
     $xml=simplexml_load_file($feed);
 
+    $guid_Check = array();
     for($i=0; $i<$limit; $i++){
         
         $category = $xml->channel->item[$i]->category;
@@ -18,16 +19,30 @@
         $endTime = $xml->channel->item[$i]->endTime;
         $description = $xml->channel->item[$i]->description;
         $guid = $xml->channel->item[$i]->guid;
-
-        echo '<a href="'.$guid.'" class="events-carousel-item"><h4>'.$category.'</h4><h3>'.$title.'</h3><div class="date-time">'.$date.'<br/>'.$startTime;
-        if($endTime != ""){
-            echo ' - '.$endTime; 
+        
+        /* Checking to see if the value guid is in the array not in the array if it isnt don't output the event. */
+        /* Events with mulitple categories will only display once */    
+        if (!(in_array((string)$guid, $guid_Check))) {
+            echo '<a href="'.$guid.'" class="events-carousel-item" id="event_carousel_item"><h4>'.$category.'</h4><h3>'.$title.'</h3><div class="date-time">'.$date.'<br/>';
+            if($endTime != ''){
+                if(strcmp($startTime, $endTime) === 0 ){
+                    echo 'All Day';
+                } else {
+                    echo $startTime.' - '.$endTime; 
+                }
+            } else {
+                if($startTime == '12:00 AM'){
+                    echo 'All Day';
+                }else{
+                    echo $startTime;
+                }
+            }
+            if($location != ""){
+                echo '<span class="location"><i class="fa fa-map-marker"></i>&nbsp;'.$location.'</span>';
+            }
+            echo '</div><p>'.$description.'</p></a>';
+            /** Adding next guid to the guid Check array */
+            $guid_Check[] = (string)$guid;
         }
-        if($location != ""){
-            echo '<span class="location"><i class="fa fa-map-marker"></i>&nbsp;'.$location.'</span>';
-        }
-        echo '</div><p>'.$description.'</p></a>';
-
     }
-
 ?>
