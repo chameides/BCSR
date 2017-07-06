6c6e3fc629cd1ff766fa30ec9dcb54ffed243719
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2017-07-06 16:26:34 -0400

    xsl directory styling

---
+++ directory2.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simon's Rock Templates, and Style Guide</title>
+
+<?php include '_inc/head-js-css.php'; ?>
+
+  </head>
+  <body>
+    <?php include '_inc/nav.php'; ?>
+    <div class="container-fluid page-container">
+         <div class="row-full-width">
+            <div class="column-left side-nav" data-spy="affix" data-offset-top="100">
+               <div class="reveal-side-nav"></div>
+               
+            </div>
+            <div class="column-center">
+               <ol class="flat-list crumb">
+                  <li><a href="/">Home</a></li>
+                  <li>&nbsp;&gt;&nbsp;</li>
+                  <li><a href="/why-simons-rock/">Why Simons Rock</a></li>
+                  <li>&nbsp;&gt;&nbsp;</li>
+                  <li><a href="/why-simons-rock/contact-us/">Contact Us</a></li>
+                  <li>&nbsp;&gt;&nbsp;</li>
+                  <li>Offices And Departments</li>
+               </ol>
+               <h1 class="header-page text-center">Offices And Departments</h1>
+                  <p>One of the best things about our administration and support offices is their dedication
+                     to our mission and our students. Below, you’ll find main phone numbers and links to
+                     individual departments on campus, where you can connect with staff members and learn
+                     more about services provided by each office.
+                  </p>
+
+                  <div id="directory-list">
+        <div class="row row-margin-bottom-none">
+          <div class="col-sm-6">
+            <input type="text" class="fuzzy-search form-control" placeholder="Search faculty and staff" />
+            <div class="image-title"><a href="https://lock.simons-rock.edu/fsdir1/">Staff by Department</a> | <a href="https://lock.simons-rock.edu/fsdir1/index.php?state=search&amp;input=all&amp;atoz=1">A-Z Faculty and Staff</a></div>
+          </div>
+        </div>
+
+                  
+                  
+                  <p class="text-center"><strong>Looking for a specific person?</strong></p>
+                 
+                  <div class="text-center margin-bottom"><a class="right-arrow-primary" href="https://lock.simons-rock.edu/fsdir/index.php?state=search&amp;input=all&amp;atoz=1"><i class="fa fa-angle-right bold"></i>&nbsp;A-Z Faculty/Staff Directory</a></div>
+                  <h2>Connect with campus departments</h2>
+                  <div class="row margin-bottom">
+                     <div class="col-sm-6">
+                        <h4 class="spotlight">Main Switchboard</h4>
+                        <p>413-644-4400</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/academic-affairs/index.php">Academic Affairs</a></h4>
+                        <p>413-528-7247</p>
+                        <h4><a href="/admission/index.php">Admission</a></h4>
+                        <p>800-235-7186 or 413-528-7228</p>
+                        <h4><a href="http://bookstore.mbsdirect.net/simons-rock.htm">Book Store</a></h4>
+                        <p>800-325-3252 (MBS Direct)</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/business-office.php">Business Office&nbsp;</a></h4>
+                        <p>413-644-4285 or 413-528-7391</p>
+                        <h4><a href="/academics/career-development/index.php">Career Development</a></h4>
+                        <p>413-528-7273</p>
+                        <h4><a href="/student-life/campus-experience/space-rentals.php">Facilities/Campus Rentals</a></h4>
+                        <p>413-644-4220</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/marketing-and-communications.php">Marketing and Communications</a></h4>
+                        <p>413-528-7209 or 413-528-7229</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/counseling-services.php">Counseling Services</a></h4>
+                        <p>413-528-7353</p>
+                        <h4><a href="/student-life/living-on-campus/dining.php">Dining Hall</a></h4>
+                        <p>413-528-7290</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/financial-aid-office.php">Financial Aid</a></h4>
+                        <p>413-528-7297</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/health-services.php">Health Services</a></h4>
+                        <p>413-528-7353&nbsp;</p>
+                     </div>
+                     <div class="col-sm-6">
+                        <h4 class="spotlight"><a href="/why-simons-rock/contact-us/offices-and-departments/information-technology-services.php">Information Technology Services</a></h4>
+                        <p>413-528-7371</p>
+                        <h4><a href="/giving/index.php">Institutional Advancement</a></h4>
+                        <p>413-528-7266</p>
+                        <h4><a href="/student-life/campus-experience/kilpatrick-athletic-center/index.php">Kilpatrick Athletic Center</a></h4>
+                        <p>413-528-7777</p>
+                        <h4><a href="/student-life/campus-experience/alumni-library/index.php">Library</a></h4>
+                        <p>413-528-7370</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/marketing-and-communications.php">Media Contact</a></h4>
+                        <p>413-528-7229</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/business-office.php">Payroll and Benefits</a></h4>
+                        <p>413-528-7205</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/physical-plant.php">Physical Plant</a></h4>
+                        <p>413-528-7208</p>
+                        <h4><a href="/why-simons-rock/contact-us/administration/peter-laipson.php">Provost's Office</a></h4>
+                        <p>413-528-7239</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/registrar.php">Registrar</a></h4>
+                        <p>413-528-7201</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/security/index.php">Security</a></h4>
+                        <p>413-528-7291</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/student-accounts.php">Student Accounts</a></h4>
+                        <p>413-644-4285</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/student-affairs.php">Student Affairs</a></h4>
+                        <p>413-528-7693</p>
+                        <h4><a href="/why-simons-rock/contact-us/offices-and-departments/win-commons.php">Win Student Resource Commons</a></h4>
+                        <p>413-528-7444</p>
+                     </div>
+                  </div>
+               
+            </div>
+    <div class="container">
+      <div class="row">
+				<p><strong>The below is iframed content, demonstrating an implementation of third-party content on site:</strong></p>
+        
+       
+      </div>
+    </div>
+    <p></p>
+
+
+
+    </div><!-- /.container -->
+
+<?php include '_inc/footer.php'; ?>
+    
+<?php include '_inc/bottom-js.php' ?>
+
+  </body>
+</html>
