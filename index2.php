7d9897ea1b8e8a69c8a646681b9e75d9e80d768e
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2018-01-08 16:12:28 -0500

    tab order script

--- index2.php
+++ index2.php
@@ -516,7 +516,48 @@
   	<script type="text/javascript" src="/_js/scrollmagic/jquery.scrollmagic.js"></script>
   	<!--<script type="text/javascript" src="/_js/scrollmagic/jquery.scrollmagic.debug.js"></script>-->
 
-	<script>
+  	<script>
+		//When mobile menu is expanded, prevent tabbing outside of menu
+		
+		function SetTabIndex ( select, value ) {
+	    //select is class or id that should be altered
+	    //value is the tab index
+
+	    //value is already set, reset
+	    if ($(select).attr('tabindex') == value) {
+	    	$(select).attr('tabindex', 0);
+	    }
+	    //value is not set, set
+	    else {
+	    	$(select).attr('tabindex', value);
+	    }
+		}
+
+		var SetTabIndexTrigger = '.navbar-toggle';
+
+		//1. when mobile menu button is toggled
+		$(SetTabIndexTrigger).click(function(){
+			
+			//2. set mobile nav elements tabindex as priority
+			
+			//Set nav button, starting point for tabbing
+			SetTabIndex(SetTabIndexTrigger,'1'); 
+
+			//Search Form
+			SetTabIndex('.input-group','2');
+
+			//Set Nav Information Architecure Links
+			SetTabIndex('.dropdown-toggle','2');
+
+			//Set CTAs
+			SetTabIndex('.nav-cta','3');
+
+			//Set CTAs
+			SetTabIndex('.info-for-mobile a','4');
+		});
+		
+
+	
 
 		//video pause/play controls for autoplay video
 		//backgroundvideocontrolsindex
