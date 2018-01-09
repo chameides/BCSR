a2ab6a3ba63c2942b896025395273d6ec1a191df
MC (laptop) <mchameides@ITSs-MacBook-Pro-37.local>
2018-01-09 13:13:35 -0500

    Reputation colors

--- style-guide.php
+++ style-guide.php
@@ -67,6 +67,7 @@
 						<li><a href="master-section-landing.php">Master Section Landing</a></li>
 						<li><a href="profile.php">Profile</a></li>
 						<li><a href="basic-profile.php">Profile (basic)</a></li>
+						<li><a href="reputations-and-outcomes.php">Reputation</a></li>
 					</ul>
 				</div>
 				<div class="col-md-4">
