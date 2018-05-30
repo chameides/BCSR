33c6f323a7836f14f37479083cb32f8e0a0bf671
“Michael <contactmeviainternet@gmail.com>
2018-05-30 16:06:36 -0400

    conform layout

--- _inc/grid.php
+++ _inc/grid.php
@@ -1,76 +1,121 @@
 <div class="grid ">
-  <div class="item item-with-link" tabindex="1">
+  <div class="item item-with-link style-image-none" tabindex="1">
     <a href="https://simons-rock.edu/news">
-    <h2>Everyone Has a Story To Tell</h2>
-    <p>Professor Jennifer Browdy’s guide to writing memoir wins Nautilus Award</p>
+      <div class="item-text">
+        <h2>Everyone Has a Story To Tell</h2>
+        <p>Professor Jennifer Browdy’s guide to writing memoir wins Nautilus Award</p>
+      </div>
     </a>
+    
   </div> 
   <div class="item style-image style-image-size4 item-with-link" tabindex="2">
-    <div class="image-container-size4" id="image-container-2">
-    </div> 
-    <div class="item-text">
-      <h2 class="caption">Austria on a Fulbright</h2>
-    </div>
+    <a href="https://simons-rock.edu/news">
+      <div class="image-container-size4" id="image-container-2">
+      </div> 
+      <div class="item-text">
+        <h2 class="caption">Austria on a Fulbright</h2>
+      </div>
+    </a>
   </div>
  <div class="item style-image item-with-link style-image-size3" tabindex="3">
-    <div class="image-container-size3" id="image-container-2">
-    </div> 
-    <div class="item-text">
-      <h2>3. headline goes here</h2>
-    </div>
+    <a href="https://simons-rock.edu/news">
+      <div class="image-container-size3" id="image-container-2">
+      </div> 
+      <div class="item-text">
+        <h2>3. headline goes here</h2>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="4">
-    <div class="item-text">
-      <h2>4. Headline</h2>
-      <h3>SubheadliNe goes here</h3>
-    </div>
+  <div class="item item-with-link style-image-none" tabindex="4">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>4. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item style-image image-split item-with-link" tabindex="5">
+  <div class="item style-image style-image-split item-with-link" tabindex="5">
+    <a href="https://simons-rock.edu/news">
       <div class="col-xs-6 image-container-size5" id="image-container-2">
       </div>
-      <div class="col-xs-6">
+      <div class="col-xs-6 item-text">
         <h2>5. Headline</h2>
-        <h3>subheadline</h3>
+        <p>subheadline</p>
       </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="6">
-    <h2>6. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item item-with-link style-image-none" tabindex="6">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>6. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link style-image-size-r2-4 style-image" tabindex="7">
-    <div class="image-container-size4" id="image-container-2">
-    </div>
-    <div class="item-text"> 
-      <h2 class="caption">Teen Vogue Writer and Political Activist Lauren Duca to Deliver Commencement Speech at Bard College at Simon's Rock</h2>
-    </div>
+  <div class="item item-with-link style-image-size-r2-3 style-image" tabindex="7">
+    <a href="https://simons-rock.edu/news">
+      <div class="image-container-size3" id="image-container-2">
+      </div>
+      <div class="item-text"> 
+        <h2 class="caption">Teen Vogue Writer and Political Activist Lauren Duca to Deliver Commencement Speech at Bard College at Simon's Rock</h2>
+      </div>
+    </a>
   </div>
-  <div class="item style-image item-with-link" tabindex="7">
-    <div class="image-container-size4" id="image-container-2">
-    </div> 
-    <h2>8. headliNe goes here</h2>
+  <div class="item style-image item-with-link style-image-size-r2-4" tabindex="7">
+    <a href="https://simons-rock.edu/news">
+      <div class="image-container-size4" id="image-container-2">
+      </div> 
+      <div class="item-text">
+        <h2>8. headliNe goes here</h2>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="9">
-    <h2>9. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item item-with-link style-image-none" tabindex="9">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>9. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="10">
-    <h2>10. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item item-with-link style-image-none" tabindex="10">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>10. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="11">
-    <h2>11. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item item-with-link style-image-none" tabindex="11">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>11. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="12">
-    <h2>12. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item item-with-link style-image-none" tabindex="12">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>12. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item item-with-link" tabindex="13">
-    <h2>13. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item item-with-link style-image-none" tabindex="13">
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>13. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
-  <div class="item" tabindex="14">
-    <h2>14. Headline</h2>
-    <h3>SubheadliNe goes here</h3>
+  <div class="item style-image-none" tabindex="14>
+    <a href="https://simons-rock.edu/news">
+      <div class="item-text">
+        <h2>14. Headline</h2>
+        <p>SubheadliNe goes here</p>
+      </div>
+    </a>
   </div>
 </div>
