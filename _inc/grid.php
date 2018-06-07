c6d0b6ac8e52b43c4cc05c90b62241f193b38861
“Michael <contactmeviainternet@gmail.com>
2018-06-07 12:41:00 -0400

    grid side scroll inside container/viewport

--- _inc/grid.php
+++ _inc/grid.php
@@ -1,122 +1,127 @@
-<div class="grid ">
-  <div class="item item-with-link style-image-none" tabindex="1">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>Everyone Has a Story To Tell</h2>
-        <p>Professor Jennifer Browdy’s guide to writing memoir wins Nautilus Award</p>
-      </div>
-    </a>
-    
-  </div> 
-  <div class="item style-image style-image-size4 item-with-link" tabindex="2">
-    <a href="https://simons-rock.edu/news">
-      <div class="image-container-size4" id="image-container-2">
-      </div> 
-      <div class="item-text">
-        <h2 class="caption">Austria on a Fulbright</h2>
-      </div>
-    </a>
-  </div>
- <div class="item style-image item-with-link style-image-size3" tabindex="3">
-    <a href="https://simons-rock.edu/news">
-      <div class="image-container-size3" id="image-container-2">
-      </div> 
-      <div class="item-text">
-        <h2>3. headline goes here</h2>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-none" tabindex="4">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>4. Headline</h2>
-        <p>SubheadliNe goes here</p>
-      </div>
-    </a>
-  </div>
-  <div class="item style-image style-image-split item-with-link" tabindex="5">
-    <a href="https://simons-rock.edu/news">
-      <div class="col-xs-6 image-container-size5" id="image-container-2">
-      </div>
-      <div class="col-xs-6 item-text">
-        <h2>5. Headline</h2>
-        <p>subheadline</p>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-none" tabindex="6">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>6. Headline</h2>
-        <p>SubheadliNe goes here</p>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-size-r2-3 style-image" tabindex="7">
-    <a href="https://simons-rock.edu/news">
-      <div class="image-container-size3" id="image-container-2">
-      </div>
-      <div class="item-text"> 
-        <h2 class="caption">Teen Vogue Writer and Political Activist Lauren Duca to Deliver Commencement Speech at Bard College at Simon's Rock</h2>
-      </div>
-    </a>
-  </div>
-  <div class="item style-image item-with-link style-image-size-r2-4" tabindex="7">
-    <a href="https://simons-rock.edu/news">
-      <div class="image-container-size4" id="image-container-2">
-      </div> 
-      <div class="item-text">
-        <h2>8. headliNe goes here</h2>
-      </div>
-    </a>
-  </div>
-  <div class="item style-image style-image-size2 item-with-link" tabindex="2">
-    <a href="https://simons-rock.edu/news">
-      <div class="image-container-size2" id="image-container-2">
-      </div> 
-      <div class="item-text">
-        <p>Professor Jennifer Browdy's guide to writing memior wins Nautilus award</p>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-none" tabindex="10">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>10. Headline</h2>
-        <p>SubheadliNe goes here</p>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-none" tabindex="11">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>11. Headline</h2>
-        <p>SubheadliNe goes here</p>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-none" tabindex="12">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>12. Headline</h2>
-        <p>SubheadliNe goes here</p>
-      </div>
-    </a>
-  </div>
-  <div class="item item-with-link style-image-none" tabindex="13">
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2 class="small">13. Headline - text goes smaller if text is really, really long.</h2>
-        <p class="small">Subheadline can also go small, if text is really long, but there is an accessibility issue. but, it's just a fallback and I think more accessible than if text gets cut off</p>
-      </div>
-    </a>
-  </div>
-  <div class="item style-image-none" tabindex="14>
-    <a href="https://simons-rock.edu/news">
-      <div class="item-text">
-        <h2>14. Headline</h2>
-        <p>SubheadliNe goes here</p>
-      </div>
-    </a>
-  </div>
-</div>
+<section id="news-grid">
+  <div class="container-fluid">
+    <div class="grid-wrapper row">
+      <div class="grid">
+        <div class="item item-with-link style-image-none" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Everyone Has a Story To Tell</h2>
+              <p>Professor Jennifer Browdy’s guide to writing memoir wins Nautilus Award</p>
+            </div>
+          </a>        
+        </div> 
+        <div class="item style-image style-image-size4 item-with-link" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size4" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2 class="caption">Austria on a Fulbright</h2>
+            </div>
+          </a>
+        </div>
+       <div class="item style-image item-with-link style-image-size3" tabindex="3">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2>3. headline goes here</h2>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-none" tabindex="4">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>4. Headline</h2>
+              <p>SubheadliNe goes here</p>
+            </div>
+          </a>
+        </div>
+        <div class="item style-image style-image-split item-with-link" tabindex="5">
+          <a href="https://simons-rock.edu/news">
+            <div class="col-xs-6 image-container-size5" id="image-container-2">
+            </div>
+            <div class="col-xs-6 item-text">
+              <h2>5. Headline</h2>
+              <p>subheadline</p>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-none" tabindex="6">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>6. Headline</h2>
+              <p>SubheadliNe goes here</p>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-size-r2-3 style-image" tabindex="7">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Teen Vogue Writer and Political Activist Lauren Duca to Deliver Commencement Speech at Bard College at Simon's Rock</h2>
+            </div>
+          </a>
+        </div>
+        <div class="item style-image item-with-link style-image-size-r2-4" tabindex="7">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size4" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2>8. headliNe goes here</h2>
+            </div>
+          </a>
+        </div>
+        <div class="item style-image style-image-size2 item-with-link" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <p>Professor Jennifer Browdy's guide to writing memior wins Nautilus award</p>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-none" tabindex="10">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>10. Headline</h2>
+              <p>SubheadliNe goes here</p>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-none" tabindex="11">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>11. Headline</h2>
+              <p>SubheadliNe goes here</p>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-none" tabindex="12">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>12. Headline</h2>
+              <p>SubheadliNe goes here</p>
+            </div>
+          </a>
+        </div>
+        <div class="item item-with-link style-image-none" tabindex="13">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2 class="small">13. Headline - text goes smaller if text is really, really long.</h2>
+              <p class="small">Subheadline can also go small, if text is really long, but there is an accessibility issue. but, it's just a fallback and I think more accessible than if text gets cut off</p>
+            </div>
+          </a>
+        </div>
+        <div class="item style-image-none" tabindex="14>
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>14. Headline</h2>
+              <p>SubheadliNe goes here</p>
+            </div>
+          </a>
+        </div><!--end item-->
+      </div><!--end grid-->
+    </div><!--end grid wrapper-->
+  </div><!--end container fluid-->
+</section>
