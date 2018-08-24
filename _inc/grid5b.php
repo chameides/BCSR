f2e4051e0f4e3d02f8041d2aee1be634ed5ae83e
“Michael <contactmeviainternet@gmail.com>
2018-08-24 12:15:12 -0400

    top 5, better

---
+++ _inc/grid5b.php
@@ -0,0 +1,213 @@
+<section id="news-grid">
+  <div class="container-fluid">
+    <div class="grid-wrapper row">
+      <div class="grid grid-5b">
+      <!-- 1x1 full image -->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size5" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-3">
+            </div> 
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-size2 news-item-link news-item-size-lg-r2-2" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-4">
+            </div> 
+            <div class="item-text">
+              <h2>Bread and Puppet</h2>
+              <p>Thursday, September 13 5:30 PM</p>
+              <p class="item-extra">Featuring giant dwarves, celestial grasshoppers, and the hot sounds of the Bread and Puppet Circus Band.</p>
+         
+            </div>
+          </a>
+        </div>
+
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-size2 news-item-link news-item-size-lg-r2-2" tabindex="3">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2>Leading the Way for Women in STEM</h2>
+              <p class="item-extra">Phyu Hninn Nyein ’05 Honored by Women of the Future Awards, South East Asia</p>
+            </div>
+          </a>
+        </div>
+
+
+        <!-- headline and text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="4">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Austria on a Fulbright</h2>
+              <p>Elibba Dean ’13 will be teaching in secondary schools while researching and taking university courses. </p>
+            </div>
+          </a>        
+        </div>  
+         <!-- event -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="5">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Berkshire Pulse Performance</h2>
+              <p>May 26 - 27 3:00 PM<br>
+                Daniel Arts Center</p>
+              <p class="item-extra">Celebrate our local artistic community with dancers and musicians.</p>
+            </div>
+          </a>
+        </div>
+        
+        <!--batch 2-->
+        <!-- 1x1 full image -->
+        <div class="news-item news-item-batch2  news-item-link news-item-image-size5" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-3">
+            </div> 
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch2  news-item-image-size2 news-item-link news-item-size-lg-r2-2" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-4">
+            </div> 
+            <div class="item-text">
+              <h2>Bread and Puppet</h2>
+              <p>Thursday, September 13 5:30 PM</p>
+              <p class="item-extra">Featuring giant dwarves, celestial grasshoppers, and the hot sounds of the Bread and Puppet Circus Band.</p>
+         
+            </div>
+          </a>
+        </div>
+
+        <!-- image with heading -->
+        <div class="news-item news-item-batch2  news-item-image-size2 news-item-link news-item-size-lg-r2-2" tabindex="3">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2>Leading the Way for Women in STEM</h2>
+              <p class="item-extra">Phyu Hninn Nyein ’05 Honored by Women of the Future Awards, South East Asia</p>
+            </div>
+          </a>
+        </div>
+
+
+        <!-- headline and text -->
+        <div class="news-item news-item-batch2 news-item-link news-item-image-none" tabindex="4">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Austria on a Fulbright</h2>
+              <p>Elibba Dean ’13 will be teaching in secondary schools while researching and taking university courses. </p>
+            </div>
+          </a>        
+        </div>  
+         <!-- event -->
+        <div class="news-item news-item-batch2 news-item-link news-item-image-none" tabindex="5">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Berkshire Pulse Performance</h2>
+              <p>May 26 - 27 3:00 PM<br>
+                Daniel Arts Center</p>
+              <p class="item-extra">Celebrate our local artistic community with dancers and musicians.</p>
+            </div>
+          </a>
+        </div>
+
+
+        <!--batch 3-->
+        
+        <!-- 1x1 full image -->
+        <div class="news-item news-item-batch2  news-item-link news-item-image-size5" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-3">
+            </div> 
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch2  news-item-image-size2 news-item-link news-item-size-lg-r2-2" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-4">
+            </div> 
+            <div class="item-text">
+              <h2>Bread and Puppet</h2>
+              <p>Thursday, September 13 5:30 PM</p>
+              <p class="item-extra">Featuring giant dwarves, celestial grasshoppers, and the hot sounds of the Bread and Puppet Circus Band.</p>
+         
+            </div>
+          </a>
+        </div>
+
+        <!-- image with heading -->
+        <div class="news-item news-item-batch2  news-item-image-size2 news-item-link news-item-size-lg-r2-2" tabindex="3">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2>Leading the Way for Women in STEM</h2>
+              <p class="item-extra">Phyu Hninn Nyein ’05 Honored by Women of the Future Awards, South East Asia</p>
+            </div>
+          </a>
+        </div>
+
+
+        <!-- headline and text -->
+        <div class="news-item news-item-batch2 news-item-link news-item-image-none" tabindex="4">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Austria on a Fulbright</h2>
+              <p>Elibba Dean ’13 will be teaching in secondary schools while researching and taking university courses. </p>
+            </div>
+          </a>        
+        </div>  
+         <!-- event -->
+        <div class="news-item news-item-batch2 news-item-link news-item-image-none" tabindex="5">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Berkshire Pulse Performance</h2>
+              <p>May 26 - 27 3:00 PM<br>
+                Daniel Arts Center</p>
+              <p class="item-extra">Celebrate our local artistic community with dancers and musicians.</p>
+            </div>
+          </a>
+        </div>
+
+
+
+
+       <!-- CTA -->
+      <div class="news-item news-item-batch1 news-item-link news-item-image-none news-item-cta1 background-red" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>View All News and Events</h2>
+            </div>
+          </a>        
+        </div>
+
+
+         <!-- CTA -->
+        <div class="news-item news-item-batch2 news-item-link news-item-image-none news-item-cta2 background-red" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>View All News and Events</h2>
+            </div>
+          </a>        
+        </div>
+
+
+        <!-- CTA -->
+        <div class="news-item news-item-batch2 news-item-link news-item-image-none news-item-cta3 background-red" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>View All News and Events</h2>
+            </div>
+          </a>        
+        </div>
+        <!-- end CTA-->
+
+
+      </div><!--end grid-->
+    </div><!--end grid wrapper-->
+      <button class="btn btn-link rockers_btn leftbtn" id="news-carousel-left"><i class="fa fa-chevron-left"></i></button>
+      <button class="btn btn-link rockers_btn rightbtn" id="news-carousel-right"><i class="fa fa-chevron-right"></i></button>
+  </div><!--end container fluid-->
+</section>
