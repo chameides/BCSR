2e5374cf91d2a02572f82d79fca1acd2704a14cd
“Michael <contactmeviainternet@gmail.com>
2018-08-31 14:51:23 -0400

    grid vertical scroll (small screen)

---
+++ _inc/grid-content-example.php
@@ -0,0 +1,250 @@
+
+        <!-- headline and text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Everyone Has a Story To Tell</h2>
+              <p>Professor Jennifer Browdy’s guide to writing memoir wins Nautilus Award</p>
+            </div>
+          </a>        
+        </div> 
+        <!-- headline and text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Leading the Way for Women in STEM</h2>
+              <p>Phyu Hninn Nyein ’05 Honored by Women of the Future Awards, South East Asia</p>
+            </div>
+          </a>        
+        </div> 
+        <!-- headline and text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="3">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Austria on a Fulbright</h2>
+              <p>Elibba Dean ’13 will be teaching in secondary schools while researching and taking university courses. </p>
+            </div>
+          </a>        
+        </div> 
+        <!-- event -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="4">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Berkshire Pulse Performance</h2>
+              <p>May 26 - 27 3:00 PM<br>
+                Daniel Arts Center</p>
+              <p class="item-extra">Celebrate our local artistic community with dancers and musicians.</p>
+            </div>
+          </a>
+        </div>
+        <!-- 2 wide image + caption -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-size5" tabindex="5">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-2">
+            </div>
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-size2 news-item-link" tabindex="6">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2 class="caption">Everyone Has a Story To Tell</h2>
+            </div>
+          </a>
+        </div>
+        <!-- image with caption -->
+        <div class="news-item news-item-batch1  news-item-image-size3 news-item-link" tabindex="7">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <p class="caption">Austria on a Fullbright</p>
+            </div>
+          </a>
+        </div>
+        <!-- 2 x 2 image and headline -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-r2-3 " tabindex="8">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Teen Vogue Writer and Political Activist Lauren Duca to Deliver Commencement Speech at Bard College at Simon's Rock</h2>
+            </div>
+          </a>
+        </div>
+        <!-- 1x1 full image -->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size5" tabindex="9">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-2">
+            </div> 
+          </a>
+        </div>
+        <!-- image with longer small text -->
+        <div class="news-item news-item-batch1  news-item-image-size-r2-2 news-item-link" tabindex="10">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <p>Phyu Hninn Nyein ’05 Honored by Women of the Future Awards, South East Asia</p>
+            </div>
+          </a>
+        </div>
+        <!--2x1 full image -->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size5" tabindex="11">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-2">
+            </div> 
+          </a>
+        </div>
+        <div class="news-item news-item-batch1 news-item-link news-item-size-r2" tabindex="12">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Everyone Has A Story To Tell</h2>
+              <p>Professor Jennifer Browdy's guide to writing memroir wins Nautilus Award</p>
+            </div>
+          </a>
+        </div>
+        <!-- reduced headline text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="13">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2 class="small">13. Headline - text goes smaller if text is really, really long.</h2>
+              <p class="small">Subheadline can also go small, if text is really long, but there is an accessibility issue. but, it's just a fallback and I think more accessible than if text gets cut off</p>
+            </div>
+          </a>
+        </div>
+
+        <!-- image 2 tall-->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size3" tabindex="14">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Lauren Duca to Deliver Commencement Speech</h2>
+            </div>
+          </a>
+        </div>
+        
+        <!--2x2 full image -->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size5" tabindex="15">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-2">
+            </div> 
+          </a>
+        </div>
+        <!--1x2 full image -->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size5" tabindex="16">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size5" id="image-container-2">
+            </div> 
+          </a>
+        </div>
+        <!-- 1x2 full text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none news-item-image-none" tabindex="17">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Everyone Has A Story To Tella</h2>
+              <p>Professor Jennifer Browdy's guide to writing memroir wins Nautilus Award</p>
+            </div>
+          </a>
+        </div>
+        <!-- 2x2 full text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none news-item-size-r2" tabindex="18">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Everyone Has A Story To Tellb</h2>
+              <p>Professor Jennifer Browdy's guide to writing memroir wins Nautilus Award</p>
+            </div>
+          </a>
+        </div>
+
+
+        <!-- image 2 tall-->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size1" tabindex="19">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size1" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Size 1, x1</h2>
+            </div>
+          </a>
+        </div>
+
+        <!-- image 2 tall-->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size2" tabindex="20">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Size 2, x1</h2>
+            </div>
+          </a>
+        </div>
+        <!-- image 2 tall-->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size3" tabindex="21">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Size 3, x1</h2>
+            </div>
+          </a>
+        </div>
+        <!-- image 2 tall-->
+        <div class="news-item news-item-batch1  news-item-link news-item-image-size4" tabindex="22">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size4" id="image-container-2">
+            </div>
+            <div class="item-text"> 
+              <h2 class="caption">Size 4, x1</h2>
+            </div>
+          </a>
+        </div>
+
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-r2-1 news-item-link" tabindex="23">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size1" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2 class="caption">Size 1, x2</h2>
+            </div>
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-r2-2 news-item-link" tabindex="24">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size2" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2 class="caption">Size 2, x2</h2>
+            </div>
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-r2-3 news-item-link" tabindex="25">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size3" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2 class="caption">Size 3, x3</h2>
+            </div>
+          </a>
+        </div>
+        <!-- image with heading -->
+        <div class="news-item news-item-batch1  news-item-image-r2-4 news-item-link" tabindex="26">
+          <a href="https://simons-rock.edu/news">
+            <div class="image-container-size4" id="image-container-2">
+            </div> 
+            <div class="item-text">
+              <h2 class="caption">Size 4, x2</h2>
+            </div>
+          </a>
+        </div>
+
+        <!--end batch 1-->
+
+
+
