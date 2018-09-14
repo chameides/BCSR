abfff71426ee19fa24192e5090e467ac24134e47
“Michael <contactmeviainternet@gmail.com>
2018-09-14 11:52:25 -0400

    add error tracking for event submission form

---
+++ events/inc/functions/datalayer-error.php
@@ -0,0 +1,7 @@
+<script>
+    //Fires an event in Google Analytics
+    dataLayer.push({'event': 'errorEventSubmission' });
+</script>
+<h4>There was an error in the form submission</h4>
+<p>Please email event info to <a href="mailto:eventrequest@simons-rock.edu">eventrequest@simons-rock.edu</a>. We apologize for the inconvenience.</p>
+<hr>
