7546a5a117bdbd9de981ff271e8c3c1c8026b539
“Michael <contactmeviainternet@gmail.com>
2018-09-24 09:11:55 -0400

    grid examples

---
+++ _inc/grid-content-5.php
@@ -0,0 +1,57 @@
+
+        <!-- headline and text -->
+        <div class="news-item
+                    news-item-batch1 news-item-link news-item-image-none" tabindex="1">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Everyone Has a Story To Tell</h2>
+              <p>Professor Jennifer Browdy’s guide to writing memoir wins Nautilus Award</p>
+            </div>
+          </a>        
+        </div> 
+         <!-- headline and text -->
+        <div class="news-item
+                    news-item-batch1 news-item-link news-item-image-none" tabindex="2">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Leading the Way for Women in STEM</h2>
+              <p>Phyu Hninn Nyein ’05 Honored by Women of the Future Awards, South East Asia</p>
+            </div>
+          </a>        
+        </div>
+        <!-- headline and text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="3">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Austria on a Fulbright</h2>
+              <p>Elibba Dean ’13 will be teaching in secondary schools while researching and taking university courses. </p>
+            </div>
+          </a>        
+        </div>  
+         <!-- event -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none" tabindex="4">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Berkshire Pulse Performance</h2>
+              <p>May 26 - 27 3:00 PM<br>
+                Daniel Arts Center</p>
+              <p class="item-extra">Celebrate our local artistic community with dancers and musicians.</p>
+            </div>
+          </a>
+        </div>
+        <!-- headline and text -->
+        <div class="news-item news-item-batch1 news-item-link news-item-image-none news-item-size-md-r2 news-item-size-lg-r2" tabindex="5">
+          <a href="https://simons-rock.edu/news">
+            <div class="item-text">
+              <h2>Item 5</h2>
+              <p>Subheadline</p>
+            </div>
+          </a>        
+        </div> 
+       
+
+
+       
+
+
+
